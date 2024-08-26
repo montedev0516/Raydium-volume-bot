e9c80cb5912d414a5191829d4ea19c2db591d0a0
Farhan Yusof <montecristodev0516@gmail.com>
2024-08-26 14:11:28 +0900

    f

--- src/createKeys.ts
+++ src/createKeys.ts
@@ -17,7 +17,6 @@ export function loadKeypairs(): Keypair[] {
   // Define a regular expression to match filenames like 'keypair1.json', 'keypair2.json', etc.
 
   return fs.readdirSync(keypairsDir)
-    .filter(file => keypairRegex.test(file)) // Use the regex to test each filename
     .map(file => {
       const filePath = path.join(keypairsDir, file);
       const secretKeyString = fs.readFileSync(filePath, { encoding: 'utf8' });
